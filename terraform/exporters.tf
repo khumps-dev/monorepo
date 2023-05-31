4c63fd39668642346ed2edb218bdb74bc19bbdf6
Kevin Humphreys <kmanh999@gmail.com>
2023-05-31 05:01:06 +0000

    add sw01 to prometheus exporter. migrated to config file

--- terraform/exporters.tf
+++ terraform/exporters.tf
@@ -35,7 +35,7 @@ resource "kubernetes_deployment_v1" "mikrotik_exporter" {
           name    = "exporter"
           image   = "nshttpd/mikrotik-exporter:1.0.12-DEVEL"
           command = ["/app/mikrotik-exporter"]
-          args    = ["-address", "$(ADDRESS)", "-device", "$(DEVICE)", "-password", "$(PASSWORD)", "-user", "$(USERNAME)"]
+          args    = ["-config-file", "/config/config.yaml"]
           port {
             container_port = 9436
           }
@@ -75,9 +75,57 @@ resource "kubernetes_deployment_v1" "mikrotik_exporter" {
               }
             }
           }
+          volume_mount {
+            mount_path = "/config"
+            name       = "exporter-config"
+          }
+        }
+        volume {
+          name = "exporter-config"
+          config_map {
+            name = kubernetes_config_map_v1.mikrotik-exporter.metadata[0].name
+          }
         }
       }
     }
+  }
+}
+
+variable "mikrotik_password" {
+  type        = string
+  description = "api password for mikrotik router"
+  sensitive   = true
+}
+
+resource "kubernetes_config_map_v1" "mikrotik-exporter" {
+  metadata {
+    generate_name = "mikrotik-exporter"
+    namespace     = "exporters"
+  }
+
+  data = {
+    "config.yaml" = yamlencode({
+      devices = [
+        {
+          name     = "r01.khumps.dev"
+          address  = "192.168.2.1"
+          user     = "prometheus"
+          password = var.mikrotik_password
+        },
+        {
+          name     = "sw01.khumps.dev"
+          address  = "192.168.2.2"
+          user     = "prometheus"
+          password = var.mikrotik_password
+        }
+      ]
+
+      features = {
+        dhcp   = true,
+        routes = true,
+        pools  = true,
+        optics = true
+    } })
   }
 }
 
